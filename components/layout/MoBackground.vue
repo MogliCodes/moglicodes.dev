5f9da3e64597e218dae9a5b560ffc10a09a4c75f
MogliCodes <moglicodes@gmail.com>
2022-07-25 19:50:04 +0200

    Add background files and component

---
+++ components/layout/MoBackground.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bg bg-color bg-gradient-to-b from-primary-lighter to-primary-darker"></div>
+    <div class="bg bg-plants"></div>
+</template>
+
+<style scoped>
+.bg-color {
+    z-index: -2;
+}
+.bg-plants {
+    z-index: -1;
+}
+
+.bg-color,
+.bg-plants {
+    @apply fixed top-0 left-0 right-0 bottom-0;
+}
+.bg-plants {
+    background-image: url("~/assets/img/bg/bgMobile.svg");
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+@media (min-width: 1024px) {
+    .bg-plants {
+        background-image: url("~/assets/img/bg/bgDesktop.svg");
+    }
+}
+
+</style>
