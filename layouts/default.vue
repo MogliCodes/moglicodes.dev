e304bf2b40943eb7b56c1b3165886e0a73708d96
MogliCodes <moglicodes@gmail.com>
2022-07-25 20:15:39 +0200

    Create app header with dark mode toggle

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
+<!--  <SiteHeader class="fixed top-0 left-0 w-full py-4" />-->
   <div class="min-h-screen">
     <slot />
   </div>
 </template>
+<script>
+import SiteHeader from "../components/layout/SiteHeader";
+export default {
+  components: {SiteHeader}
+}
+</script>
