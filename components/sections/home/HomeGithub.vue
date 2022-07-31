982cad1ca588db4bdb4a06fd46cf57599363ab8f
MogliCodes <moglicodes@gmail.com>
2022-07-31 12:37:58 +0200

    Add null safety to Github events

--- components/sections/home/HomeGithub.vue
+++ components/sections/home/HomeGithub.vue
@@ -2,7 +2,7 @@
   <section id="github-activity" class="section py-16">
     <LayoutMoContainer>
       <AtomsMoHeadline text="Latest GitHub Activity" headline-type="h1" class="text-center" />
-      <LayoutMoGrid grid-columns="2">
+      <LayoutMoGrid v-if="events" grid-columns="2">
         <MoleculesMoGithubEvent v-for="event in events.slice(0, 6)" :key="event.id" :event="event" />
       </LayoutMoGrid>
     </LayoutMoContainer>
