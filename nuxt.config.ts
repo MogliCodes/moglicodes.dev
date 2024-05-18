4a70446a5dbafe5e0457d45cf6a2dbdad60c2679
MogliCodes <moglicodes@gmail.com>
2024-05-18 18:46:25 +0200

    chore(deps): update to latest Nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     publicRuntimeConfig: {
         STRAPI_URL: process.env.STRAPI_URL,
